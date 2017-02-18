bb7c6234e67edd6432104a1360c01520600fb914
root <logicmoo@gmail.com>
2017-02-18 12:04:59 -0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # multimodal_dcg
 Reduce floundering of DCGs by constraining and narrowing search 
+
+```prolog
+
+ :- pack_install('https://github.com/TeamSPoon/multimodal_dcg.git').
+
+```
+
+
+```prolog
+
+:- use_module(library(multimodal_dcg)).
+
+
+predicate_named(Pred) --> dcgAnd(theText(Text),dcgLenBetween(1,5)).
+
+:- must(dcgAnd(dcgLenBetween(5,1),theText(_Text),[a,b,c],[])).
+:- must(predicate_named(_P,[proper,-,named],[])).
+
+
+```
---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(multimodal_dcg).
+version('0.0.1').
+title('Reduce floundering of DCGs by constraining and narrowing search').
+keywords([dcg,parsing,optimization]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/TeamSPoon/multimodal_dcg' ).
+download( 'https://github.com/TeamSPoon/multimodal_dcg/release/*.zip' ). 
