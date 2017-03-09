762fc2c3e8c5f0d9738e84021ac4f0591990148f
root <logicmoo@gmail.com>
2017-03-08 17:32:32 -0800

    v1.1.111

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(multimodal_dcg).
-version('0.0.8').
+version('1.1.111').
 title('Reduce floundering of DCGs by constraining and narrowing search').
 keywords([dcg,parsing,optimization]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
@@ -7,3 +7,4 @@ packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home( 'https://github.com/TeamSPoon/multimodal_dcg' ).
 download( 'https://github.com/TeamSPoon/multimodal_dcg/release/*.zip' ). 
+requires(logicmoo_utils).
