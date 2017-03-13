b3843d36b4cd64b31db13bcc9a8c57d0a28cc7b1
root <logicmoo@gmail.com>
2017-03-13 06:53:57 -0700

    v1.1.113

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(multimodal_dcg).
-version('1.1.112').
+version('1.1.113').
 title('Reduce floundering of DCGs by constraining and narrowing search').
 keywords([dcg,parsing,optimization]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
