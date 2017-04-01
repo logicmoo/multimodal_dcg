4f172bc6d4709971fd6fa30ad4102239ecd60f36
root <logicmoo@gmail.com>
2017-03-31 19:15:47 -0700

    rdf_rewrite:arity/2 2017-03-31-19:15:15

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(multimodal_dcg).
-version('1.1.117').
+version('1.1.118').
 title('Reduce floundering of DCGs by constraining and narrowing search').
 keywords([dcg,parsing,optimization]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
